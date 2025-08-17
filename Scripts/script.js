6f0de6a9f45583bdd3c4fc8ca6703b7b95995d66
Elvin Martinez <43117188+Elvin308@users.noreply.github.com>
2025-08-16 22:57:18 -0500

    Added resume information and created new hobby file

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,7 +1,7 @@
 function downloadResume() 
 {
     const resumeLink = document.createElement('a');
-    resumeLink.href = './assets/Resume_ElvinMartinez.pdf';
+    resumeLink.href = './assets/PDF/Resume_ElvinMartinez.pdf';
     resumeLink.download = 'Elvin_Martinez_Resume.pdf';
     document.body.appendChild(resumeLink);
     resumeLink.click();
