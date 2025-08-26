8ed939dc0113381b6a08dcdaf7ed3aa6a8b1f42a
Elvin Martinez <43117188+Elvin308@users.noreply.github.com>
2025-08-25 22:13:23 -0500

    Overhauled navigation bar

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,18 @@
 function downloadResume() 
 {
     const resumeLink = document.createElement('a');
-    resumeLink.href = './assets/PDF/Resume_ElvinMartinez.pdf';
+    resumeLink.href = '/assets/PDF/Resume_ElvinMartinez.pdf';
     resumeLink.download = 'Elvin_Martinez_Resume.pdf';
     document.body.appendChild(resumeLink);
     resumeLink.click();
     document.body.removeChild(resumeLink);
 }
+
+
+const toggleBtn = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector("header nav ul");
+
+  toggleBtn.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+    toggleBtn.textContent = (toggleBtn.textContent == "☰" ? "✖" : "☰");
+  });
