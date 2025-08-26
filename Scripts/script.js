de65ff9268df9f3ed52e50357f12475baaa30446
Elvin Martinez <43117188+Elvin308@users.noreply.github.com>
2025-08-26 00:59:43 -0500

    Rename 'About me' To summary and overhauled for new style

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,5 @@
+
+//   ************************Resume Download*******************************   //
 function downloadResume() 
 {
     const resumeLink = document.createElement('a');
@@ -9,6 +11,8 @@ function downloadResume()
 }
 
 
+//   ************************Navigation bar toggle*******************************   //
+
 const toggleBtn = document.querySelector(".menu-toggle");
 const navMenu = document.querySelector("header nav ul");
 
@@ -16,3 +20,22 @@ const navMenu = document.querySelector("header nav ul");
     navMenu.classList.toggle("show");
     toggleBtn.textContent = (toggleBtn.textContent == "☰" ? "✖" : "☰");
   });
+
+
+
+//   ************************Rain Effect*******************************   //
+
+  const rainContainer = document.getElementById('rain');
+  const numDrops = 50;
+
+    for (let i = 0; i < numDrops; i++) {
+      const drop = document.createElement('div');
+      drop.classList.add('drop');
+
+      // Random position and speed
+      drop.style.left = Math.random() * 100 + 'vw';
+      drop.style.animationDuration = 2 + Math.random() * 3 + 's';
+      drop.style.animationDelay = Math.random() * 5 + 's';
+
+      rainContainer.appendChild(drop);
+    }
